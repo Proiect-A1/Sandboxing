81332e3305607b5de22b487f50c69197f874cd05
s0chu <didididi328@gmail.com>
2026-04-03 14:05:28 +0300

    IOhelper restructure

--- headers/IO.hpp
+++ headers/IO.hpp
@@ -9,12 +9,29 @@ namespace IOhelper
     int current_pos = 0 , length = 0;
     char buff[BUFF_SIZE];
 
+    int read_consistent(int fd , void *data , int len)
+    {
+        int total_read = 0;
+
+        while(len)
+        {
+            int read_len = read(fd , data , len); if(read_len == -1) return -1;
+            if(read_len == 0) return total_read;
+            total_read += read_len;
+            data = (void *)((long long)(data) + read_len);
+            len -= read_len;
+        }
+
+        return total_read;
+    }
+
     char get_char_fd(int fd)
     {
         if(current_pos == length)
         {
             length = read(fd , buff , BUFF_SIZE);
             if(length == -1) handle_error(1 , "get_char_fd()");
+            if(length == 0) handle_error(1 , "get_char_fd()");
         }
 
         return buff[current_pos++];
