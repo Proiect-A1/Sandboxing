45d8654feae0dcd0aa3137a3199d124b212142de
Trollbert <poparobert2012@gmail.com>
2026-04-07 18:26:20 +0300

    sper sa fie tot ce trebe la enums

--- Enums/group_type_enum.h
+++ Enums/group_type_enum.h
@@ -2,9 +2,9 @@
 #define GROUP_TYPE_ENUM_H
 
 enum class group_type_enum {
-  GROUPMIN, // points awarded is the minimum of the points obtained on the tests in the group
-  GROUPSUM, // points awarded is the sum of the points obtained on the tests in
-  GROUPMUL, // points awarded is the product of the points obtained on the tests in the group
+  GROUP_MIN, // points awarded is the minimum of the points obtained on the tests in the group
+  GROUP_SUM, // points awarded is the sum of the points obtained on the tests in
+  GROUP_MUL, // points awarded is the product of the points obtained on the tests in the group
 };
 
 #endif // GROUP_TYPE_ENUM_H
