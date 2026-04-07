68e3ad1abe43b3028ef0fc9dec7fd51f14705dfe
Trollbert <poparobert2012@gmail.com>
2026-04-07 19:43:35 +0300

    bonus utilities

--- Utilities/submission_info_utilities.cpp
+++ Utilities/submission_info_utilities.cpp
@@ -9,8 +9,21 @@ std::string submission_info_utilities::left_zero_pad (int number, int width) {
   return std::string(width - num_str.length(), '0') + num_str;
 }
 
-std::string submission_info_utilities::get_submission_exec_path(std::string submission_id) {
-  return std::string(getenv("SANDBOX_PATH")) + "/submissions/" + submission_id + "/main_exec";
+
+std::string submission_info_utilities::get_submission_dir(std::string submission_id){
+  return std::string(getenv("SANDBOX_PATH")) + "/submissions/" + submission_id;
+}
+
+std::string submission_info_utilities::get_submission_source_path(std::string submission_id, language_enum language) {
+  if (language == language_enum::CPP) {
+    return std::string(getenv("SANDBOX_PATH")) + "/submissions/" + submission_id + "/main.cpp";
+  }
+}
+
+std::string submission_info_utilities::get_submission_exec_path(std::string submission_id, language_enum language) {
+  if (language == language_enum::CPP) {
+    return std::string(getenv("SANDBOX_PATH")) + "/submissions/" + submission_id + "/main_exec";
+  }
 }
 std::string submission_info_utilities::get_problem_input_path(std::string problem_id, int rev_id, int test) {
   return std::string(getenv("SANDBOX_PATH")) + "/inputs/" + problem_id + "." + std::to_string(rev_id) + "/" + left_zero_pad(test, 3) + ".in";
