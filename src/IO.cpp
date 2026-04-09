a38b817bf8c1edfb6cbba4715994d9204f5d675e
s0chu <didididi328@gmail.com>
2026-04-09 12:35:35 +0300

    memory safe

--- src/IO.cpp
+++ src/IO.cpp
@@ -8,192 +8,197 @@ using namespace std;
 const char *bd_ip = "127.0.0.1";
 const short bd_port = 6001;
 
-namespace IOhelper 
+ 
+IO::IO()
 {
-    int current_pos = 0 , length = 0;
-    char buff[BUFF_SIZE];
+    current_pos = 0 , length = 0;
+}
+    
+int IO::read_consistent(int fd , void *data , int len)
+{
+    int total_read = 0;
 
-    int read_consistent(int fd , void *data , int len)
+    while(len)
     {
-        int total_read = 0;
-
-        while(len)
-        {
-            int read_len = read(fd , data , len); if(read_len == -1) return -1;
-            if(read_len == 0) return total_read;
-            total_read += read_len;
-            data = (void *)((unsigned long long)(data) + read_len);
-            len -= read_len;
-        }
-
-        return total_read;
+        int read_len = read(fd , data , len); if(read_len == -1) return -1;
+        if(read_len == 0) return total_read;
+        total_read += read_len;
+        data = (void *)((unsigned long long)(data) + read_len);
+        len -= read_len;
     }
 
-    char get_char_fd(int fd)
-    {
-        if(current_pos == length)
-        {
-            length = read(fd , buff , BUFF_SIZE);
-            if(length == -1) handle_error(1 , "get_char_fd()");
-            if(length == 0) handle_error(1 , "get_char_fd()");
-            current_pos = 0;
-        }
+    return total_read;
+}
 
-        return buff[current_pos++];
+char IO::get_char_fd(int fd)
+{
+    if(current_pos == length)
+    {
+        length = read(fd , buff , BUFF_SIZE);
+        if(length == -1) handle_error(1 , "get_char_fd()");
+        if(length == 0) handle_error(1 , "get_char_fd()");
+        current_pos = 0;
     }
 
-    sockaddr_in prepare_ip(const char *ip , short port)
-    {
-        in_addr addr;
-        inet_aton(ip , &addr);
+    return buff[current_pos++];
+}
 
-        sockaddr_in socket_address;
-        socket_address.sin_family = AF_INET;
-        socket_address.sin_port = htons(port);
-        socket_address.sin_addr = addr;
-        return socket_address;
-    } 
+sockaddr_in IO::prepare_ip(const char *ip , short port)
+{
+    in_addr addr;
+    inet_aton(ip , &addr);
 
-    int create_socket()
+    sockaddr_in socket_address;
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = htons(port);
+    socket_address.sin_addr = addr;
+    return socket_address;
+} 
+
+int IO::create_socket()
+{
+    sockaddr_in bd_address = prepare_ip(bd_ip , bd_port);
+    int sockfd = socket(AF_INET , SOCK_STREAM  , 0); if(sockfd == -1) handle_error(1 , "socket() create_socket()")
+    if(connect(sockfd , (sockaddr *) &bd_address , sizeof(bd_address)) == -1) handle_error(1 , "connect() create_socket()");
+    return sockfd;
+}
+
+void IO::send(const char *msg , int fd)
+{
+    int length = strlen(msg);
+    if(write(fd , &length , sizeof(length)) != sizeof(length)) handle_error(1 , "write() send()");
+    if(write(fd , msg , length) != length) handle_error(1 , "write() send()")
+}
+
+string IO::recv(int fd) //need delete after usage
+{
+    int length; if(read_consistent(fd , &length , sizeof(length)) != sizeof(length)) handle_error(1 , "read_consistent()");
+    string payload;
+    payload.resize(length + 1);
+
+    if(read_consistent(fd , (void *) payload.c_str() , length) != length) handle_error(1 , "read_consistent()");
+    payload[length] = '\0';
+    return payload;
+
+}
+
+void IO::done_test_request(string submissionId , int testId , int verdict , string message , float score , float maxScore , float scorePercent , long long memory , long long time)
+{   
+    int sockfd = create_socket();
+    json request;
+    request["request"] = "doneTest";
+    request["submissionId"] = submissionId;
+    request["testId"] = testId;
+    request["verdict"] = verdict;
+    request["message"] = message;
+    request["score"] = score;
+    request["maxScore"] = maxScore;
+    request["score%"] = scorePercent;
+    request["memory"] = memory;
+    request["time"] = time;
+    IO::send(request.dump().c_str() , sockfd);
+    close(sockfd);
+}
+
+void IO::done_subtask_request(string submissionId , int subtaskId , float score , float maxScore , float scorePercent , long long maxMemory , long long maxTime)
+{
+    int sockfd = create_socket();
+    json request;
+    request["request"] = "doneSubtask";
+    request["submissionId"] = submissionId;
+    request["subtaskId"] = subtaskId;
+    request["score"] = score;
+    request["maxScore"] = maxScore;
+    request["score%"] = scorePercent;
+    request["maxMemory"] = maxMemory;
+    request["maxTime"] = maxTime; 
+    send(request.dump().c_str() , sockfd);
+    close(sockfd);
+}
+
+void IO::done_submission_request(string submissionId , float score , float maxScore , float scorePercent , long long maxMemory , long long maxTime)
+{
+    int sockfd = create_socket();
+    json request;
+    request["request"] = "doneSubmission"; 
+    request["submissionId"] = submissionId;
+    request["score"] = score;
+    request["maxScore"] = maxScore;
+    request["score%"] = scorePercent;
+    request["maxMemory"] = maxMemory;
+    request["maxTime"] = maxTime; 
+    send(request.dump().c_str() , sockfd);
+    close(sockfd);
+}
+
+void IO::upload_tests_request(string problemId , int revId , string archiveType , vector < vector < int > > groups , int archive_fd)
+{
+    int sockfd = create_socket();
+    json request;
+    request["request"] = "uploadTests";
+    request["problemId"] = problemId;
+    request["revId"] = revId;
+    request["archiveType"] = archiveType;
+    request["groups"] = groups;
+    send(request.dump().c_str() , sockfd);
+
+    int archive_length = lseek(archive_fd , 0 , SEEK_END); if(archive_length == -1) handle_error(1 , "lseek() upload_tests_request()");
+    if(lseek(archive_fd , 0 , SEEK_SET) == -1) handle_error(1 , "lseek() upload_tests_request()");;
+
+    if(write(sockfd , &archive_length , sizeof(archive_length)) != sizeof(archive_length)) handle_error(1 , "write() upload_tests()");
+
+    int length_read = 0;
+
+    while((length_read = read(archive_fd , buff , BUFF_SIZE)) != 0)
     {
-        sockaddr_in bd_address = prepare_ip(bd_ip , bd_port);
-        int sockfd = socket(AF_INET , SOCK_STREAM  , 0); if(sockfd == -1) handle_error(1 , "socket() create_socket()")
-        if(connect(sockfd , (sockaddr *) &bd_address , sizeof(bd_address)) == -1) handle_error(1 , "connect() create_socket()");
-        return sockfd;
+        if(length_read == -1) handle_error(1 , "read() upload_tests()");
+        if(write(sockfd , buff , length_read) != length_read) handle_error(1 , "write() upload_tests");
     }
 
-    void send(const char *msg , int fd)
+    close(sockfd);
+}
+
+void IO::pull_problem_request(string problemId)
+{
+    int sockfd = create_socket();
+    json request;
+    request["request"] = "pullProblem";
+    request["problemId"] = problemId;
+    send(request.dump().c_str() , sockfd);
+    close(sockfd);
+}
+
+void IO::evaluate_request(json request , int fd)
+{
+    char path[PATH_MAX];
+    sprintf(path , "%s/submissions/%s" , getenv("SANDBOX_PATH") , request["submissionId"].get < string > ().c_str());
+    int submission_fd = open(path , O_CREAT | O_TRUNC | O_RDWR , 0600);
+    int length; if(read_consistent(fd , &length , sizeof(length)) == -1) handle_error(1 , "read_consistent() evaluate_request()");
+    //need to finish
+
+    for(int i = 1 ; i <= length ; i++)
     {
-        int length = strlen(msg);
-        if(write(fd , &length , sizeof(length)) != sizeof(length)) handle_error(1 , "write() send()");
-        if(write(fd , msg , length) != length) handle_error(1 , "write() send()")
+        char byte = get_char_fd(fd);
+        if(write(submission_fd , &byte , sizeof(byte)) != sizeof(byte)) handle_error(1 , "write() send_problem_request()"); 
     }
 
-    char *recv(int fd)
+    close(submission_fd);
+}
+
+void IO::send_problem_request(json request , int fd)
+{
+    char path[PATH_MAX];
+    sprintf(path , "%s/tmp/%s.%d" , getenv("SANDBOX_PATH") , request["problemId"].get < string > ().c_str() , request["revId"].get < int > ());
+    
+    int problem_fd = open(path , O_CREAT | O_TRUNC | O_RDWR , 0600);
+    int length; if(read_consistent(fd , &length , sizeof(length)) == -1) handle_error(1 , "read_consistent() evaluate_request()");
+    //need to finish
+
+    for(int i = 1 ; i <= length ; i++)
     {
-        int length; if(read_consistent(fd , &length , sizeof(length)) != sizeof(length)) handle_error(1 , "read_consistent()");
-        char *payload = (char *) malloc(length + 1);
-        if(read_consistent(fd , payload , length) != length) handle_error(1 , "read_consistent()");
-        payload[length] = '\0';
-        return payload;
-
+        char byte = get_char_fd(fd);
+        if(write(problem_fd , &byte , sizeof(byte)) != sizeof(byte)) handle_error(1 , "write() send_problem_request()"); 
     }
 
-    void done_test_request(string submissionId , int testId , int verdict , string message , float score , float maxScore , float scorePercent , long long memory , long long time)
-    {   
-        int sockfd = create_socket();
-        json request;
-        request["request"] = "doneTest";
-        request["submissionId"] = submissionId;
-        request["testId"] = testId;
-        request["verdict"] = verdict;
-        request["message"] = message;
-        request["score"] = score;
-        request["maxScore"] = maxScore;
-        request["score%"] = scorePercent;
-        request["memory"] = memory;
-        request["time"] = time;
-        send(request.dump().c_str() , sockfd);
-        close(sockfd);
-    }
-
-    void done_subtask_request(string submissionId , int subtaskId , float score , float maxScore , float scorePercent , long long maxMemory , long long maxTime)
-    {
-        int sockfd = create_socket();
-        json request;
-        request["request"] = "doneSubtask";
-        request["submissionId"] = submissionId;
-        request["subtaskId"] = subtaskId;
-        request["score"] = score;
-        request["maxScore"] = maxScore;
-        request["score%"] = scorePercent;
-        request["maxMemory"] = maxMemory;
-        request["maxTime"] = maxTime; 
-        send(request.dump().c_str() , sockfd);
-        close(sockfd);
-    }
-
-    void done_submission_request(string submissionId , float score , float maxScore , float scorePercent , long long maxMemory , long long maxTime)
-    {
-        int sockfd = create_socket();
-        json request;
-        request["request"] = "doneSubmission"; 
-        request["submissionId"] = submissionId;
-        request["score"] = score;
-        request["maxScore"] = maxScore;
-        request["score%"] = scorePercent;
-        request["maxMemory"] = maxMemory;
-        request["maxTime"] = maxTime; 
-        send(request.dump().c_str() , sockfd);
-        close(sockfd);
-    }
-
-    void upload_tests_request(string problemId , int revId , string archiveType , vector < vector < int > > groups , int archive_fd)
-    {
-        int sockfd = create_socket();
-        json request;
-        request["request"] = "uploadTests";
-        request["problemId"] = problemId;
-        request["revId"] = revId;
-        request["archiveType"] = archiveType;
-        request["groups"] = groups;
-        send(request.dump().c_str() , sockfd);
-
-        int archive_length = lseek(archive_fd , 0 , SEEK_END); if(archive_length == -1) handle_error(1 , "lseek() upload_tests_request()");
-        if(lseek(archive_fd , 0 , SEEK_SET) == -1) handle_error(1 , "lseek() upload_tests_request()");;
-
-        if(write(sockfd , &archive_length , sizeof(archive_length)) != sizeof(archive_length)) handle_error(1 , "write() upload_tests()");
-
-        int length_read = 0;
-
-        while((length_read = read(archive_fd , buff , BUFF_SIZE)) != 0)
-        {
-            if(length_read == -1) handle_error(1 , "read() upload_tests()");
-            if(write(sockfd , buff , length_read) != length_read) handle_error(1 , "write() upload_tests");
-        }
-
-        close(sockfd);
-    }
-
-    void pull_problem_request(string problemId)
-    {
-        int sockfd = create_socket();
-        json request;
-        request["request"] = "pullProblem";
-        request["problemId"] = problemId;
-        send(request.dump().c_str() , sockfd);
-        close(sockfd);
-    }
-
-    void evaluate_request(json request , int fd)
-    {
-        int submission_fd = open(request["submissionId"].get < string > ().c_str() , O_CREAT | O_TRUNC | O_RDWR , 0600);
-        int length; if(read_consistent(fd , &length , sizeof(length)) == -1) handle_error(1 , "read_consistent() evaluate_request()");
-        //need to finish
-
-        while(length)
-        {
-            int length_read = read(fd , buff , BUFF_SIZE); if(length_read == -1) handle_error(1 , "read() evaluate_request()");
-            if(write(submission_fd , buff , length_read) != length_read) handle_error(1 , "write() evalute_request()");
-            length -= length_read;
-        }
-
-        close(submission_fd);
-    }
-
-    void send_problem_request(json request , int fd)
-    {
-        int problem_fd = open(request["problemId"].get < string > ().c_str() , O_CREAT | O_TRUNC | O_RDWR , 0600);
-        int length; if(read_consistent(fd , &length , sizeof(length)) == -1) handle_error(1 , "read_consistent() evaluate_request()");
-        //need to finish
-
-        while(length)
-        {
-            int length_read = read(fd , buff , BUFF_SIZE); if(length_read == -1) handle_error(1 , "read() evaluate_request()");
-            if(write(problem_fd , buff , length_read) != length_read) handle_error(1 , "write() evalute_request()");
-            length -= length_read;
-        }
-
-        close(problem_fd);
-    }
+    close(problem_fd);
 }
