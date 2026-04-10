48f68dcf3a13ef8b3e9b42e2edb2833b4e6249d8
Trollbert <poparobert2012@gmail.com>
2026-04-10 09:55:31 +0000

    scuzati am bagat acuma int

--- headers/Singletoni/user_queue.h
+++ headers/Singletoni/user_queue.h
@@ -7,15 +7,15 @@
 class user_queue{
   static user_queue* instance;
   static pthread_mutex_t mtx;
-  mutex_queue<std::string> queue;
+  mutex_queue<int> queue;
   user_queue(){}
   public:
   static user_queue& get_instance();
   bool empty();
   int size();
-  void push(const std::string& item);
-  std::string pop();
-  std::string front();
+  void push(const int& item);
+  int pop();
+  int front();
 };
 
 
