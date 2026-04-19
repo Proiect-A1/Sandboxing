41be0db14b8e22527dfe2142758b6037f99f302e
Your Name <you@example.com>
2026-04-19 10:23:55 +0300

    run server

---
+++ run_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $(whoami) != "root" ]
+then 
+    echo "Needs root privileges"
+    exit 2
+fi
+
+cd $SANDBOX_PATH
+
+#sudo mkdir -p $SANDBOX_PATH/bin ; sudo cp /bin/bash $SANDBOX_PATH/bin ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/bin/bash
+#sudo mkdir -p $SANDBOX_PATH/usr/bin ; sudo cp /usr/bin/ls $SANDBOX_PATH/usr/bin/ ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/ls
+#sudo bash get_dependencies.sh runs/amarat1/main_exec 
+
+cd ../
+
+g++ -I headers main.cpp src/*/* -o main && ./main 10.8.0.3 6000 15
