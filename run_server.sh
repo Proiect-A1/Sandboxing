66220364aed5e1132371a059c8734c1dd5490ddb
s0chu <didididi328@gmail.com>
2026-04-25 18:59:19 +0300

    fara sudo

--- run_server.sh
+++ run_server.sh
@@ -8,9 +8,9 @@ fi
 
 echo "SANDBOX FOUND IN: $SANDBOX_PATH"
 cd $SANDBOX_PATH
-#sudo mkdir -p $SANDBOX_PATH/bin ; sudo cp /bin/bash $SANDBOX_PATH/bin ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/bin/bash
-#sudo mkdir -p $SANDBOX_PATH/usr/bin ; sudo cp /usr/bin/ls $SANDBOX_PATH/usr/bin/ ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/ls
-sudo bash get_dependencies.sh runs/amarat1/main_exec 
+#mkdir -p $SANDBOX_PATH/bin ; sudo cp /bin/bash $SANDBOX_PATH/bin ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/bin/bash
+#mkdir -p $SANDBOX_PATH/usr/bin ; sudo cp /usr/bin/ls $SANDBOX_PATH/usr/bin/ ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/ls
+bash get_dependencies.sh runs/amarat1/main_exec 
 
 cd ../runners
 g++ -I ../headers main.cpp ../src/*/* -o main && ./main 127.0.0.1 6000 15
