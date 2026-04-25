e495636a3cd1f1f42d5ffd02051fc362fac1c82b
s0chu <didididi328@gmail.com>
2026-04-25 21:59:08 +0300

    patched tests

--- run_server.sh
+++ run_server.sh
@@ -8,8 +8,13 @@ fi
 
 echo "SANDBOX FOUND IN: $SANDBOX_PATH"
 cd $SANDBOX_PATH
-#mkdir -p $SANDBOX_PATH/bin ; sudo cp /bin/bash $SANDBOX_PATH/bin ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/bin/bash
-#mkdir -p $SANDBOX_PATH/usr/bin ; sudo cp /usr/bin/ls $SANDBOX_PATH/usr/bin/ ; sudo bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/ls
+
+#cp ../essentials/get_dependencies.sh .
+#mkdir -p $SANDBOX_PATH/bin ;  cp /bin/bash $SANDBOX_PATH/bin ;  bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/bin/bash
+#mkdir -p $SANDBOX_PATH/usr/bin ;  cp /usr/bin/ls $SANDBOX_PATH/usr/bin/ ;  bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/ls
+#mkdir -p $SANDBOX_PATH/usr/bin ;  cp /usr/bin/getfacl $SANDBOX_PATH/usr/bin/ ;  bash $SANDBOX_PATH/get_dependencies.sh $SANDBOX_PATH/usr/bin/getfacl
+#/usr/bin/getfacl
+#rm get_dependencies.sh
 
 cd ../runners
 g++ -I ../headers main.cpp ../src/*/* -o main 
