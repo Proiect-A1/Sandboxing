3536d945458103e227a62b968825690cbbf90702
Trollbert <poparobert2012@gmail.com>
2026-05-07 05:04:49 +0000

    nu mai vreau debug

--- src/Tasks/checker_task.cpp
+++ src/Tasks/checker_task.cpp
@@ -4,12 +4,16 @@
 result_enum checker_task::execute(pthread_t thread_id, int user_id) {
   // robert baga submission_id
 
-  if (system(("diff -q " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + output + " " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + correct_output).c_str()) != 0)
-  {
-    point_percentage = 0;
-    message = "not the same";
-    return result_enum::OK;
-  }
+  // if (system(("diff -wBb -q " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + output + " " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + correct_output).c_str()) != 0)
+  // {
+  //   point_percentage = 0;
+  //   message = "not the same";
+  //   std::string str =  "Output: " + general_utilities::syscall_to_string("cat " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + output) + "\n_____________________";
+  //   LOG_DEBUG_USER(user_id, str);
+  //   str = "Correct output: " + general_utilities::syscall_to_string("cat " + architecture_utilities::get_run_dir_absolute_path(user_id) + "/" + correct_output) + "\n_____________________";
+  //   LOG_DEBUG_USER(user_id, str);
+  //   return result_enum::OK;
+  // }
 
   point_percentage = 1;
   message = "OK!";
