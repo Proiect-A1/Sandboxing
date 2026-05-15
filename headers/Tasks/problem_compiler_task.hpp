ceb343c7681b35735c3c58dfbb977693131de7d2
Trollbert <poparobert2012@gmail.com>
2026-05-15 08:30:44 +0000

    fingers crossed

--- headers/Tasks/problem_compiler_task.hpp
+++ headers/Tasks/problem_compiler_task.hpp
@@ -4,15 +4,16 @@
 #include <Singletoni/logger.h>
 #include <Tasks/task.h>
 #include <Tasks/stdio_compiler_task.h>
-#include <problem_manager.h>
+#include <Tasks/stdio_compiler_factory.h>
+#include <filesystem>
+#include <Singletoni/problem_manager.h>
 class problem_compiler_task : public task {
   std::string problem_id;
   int rev_id;
-  stdio_compiler_task* compiler_task;
+  std::string source_path;
 public:
-  problem_compiler_task(const std::string& problem_id, int rev_id, stdio_compiler_task* compiler_task);
-  ~problem_compiler_task(){
-    delete compiler_task;
+  problem_compiler_task(const std::string& problem_id, int rev_id, const std::string& source_path): problem_id(problem_id), rev_id(rev_id), source_path(source_path) {
+    priority = 3; // Prioritate mai mare pentru compilarea surselor
   }
   bool check_permissions(int user_id) override {
     return true;
