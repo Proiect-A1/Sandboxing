be3ae7a9b5bbf1ca3eaf6a0c71c1416e49dd760f
s0chu <didididi328@gmail.com>
2026-05-16 19:28:04 +0300

    deployed vps

--- run_client.sh
+++ run_client.sh
@@ -5,6 +5,9 @@ g++ -I headers runners/client.cpp src/*/* -o runners/client -lseccomp -lwebsocke
 if [ $# -eq 1 -a $1 == 'server' ]
 then 
      ./runners/client 10.8.0.3 6000
+elif [ $# -eq 1 -a $1 == 'vps' ]
+then
+    ./runners/client 141.144.228.200 6000
 else 
     ./runners/client 127.0.0.1 6000
 fi
