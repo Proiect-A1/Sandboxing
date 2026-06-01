d2db64c3b4122a0aac02a263770ed713a6854f4b
Gheal06 <alexandru.gheorghies@gmail.com>
2026-06-01 12:10:12 +0300

    fix la sum

--- testing_data/sum.1/files/checkers/checker.cpp
+++
@@ -1,9 +0,0 @@
-#include "problem.h"
-
-int main(int argc, char** argv){
-    checker c(argc, argv);
-    int64_t out = c.out.readInt();
-    int64_t ok = c.ok.readInt();
-    if(out==ok) c.quitf(OK, "Correct");
-    else c.quitf(WA, "Wrong Answer");
-}
