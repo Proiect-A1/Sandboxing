379241be54414b89da3805aeb79f875fc0220626
s0chu <didididi328@gmail.com>
2026-06-02 15:11:22 +0300

    syscalls

--- src/Tasks/super_runner_task.cpp
+++ src/Tasks/super_runner_task.cpp
@@ -3,28 +3,34 @@
 
 std::map<language_enum, std::vector<int>> allowed_syscalls = {
     {language_enum::C, {
-        SCMP_SYS(brk), SCMP_SYS(close), SCMP_SYS(exit), SCMP_SYS(exit_group),
-        SCMP_SYS(faccessat), SCMP_SYS(fcntl), SCMP_SYS(fstatfs), SCMP_SYS(futex), 
-        SCMP_SYS(getpid), SCMP_SYS(getrandom), SCMP_SYS(gettid), SCMP_SYS(ioctl), 
-        SCMP_SYS(lseek), SCMP_SYS(madvise), SCMP_SYS(mmap), SCMP_SYS(mprotect), 
-        SCMP_SYS(mremap), SCMP_SYS(munmap), SCMP_SYS(newfstatat), SCMP_SYS(openat), 
+        SCMP_SYS(brk), SCMP_SYS(clock_gettime), SCMP_SYS(close), SCMP_SYS(exit), 
+        SCMP_SYS(exit_group), SCMP_SYS(faccessat), SCMP_SYS(faccessat2), SCMP_SYS(fcntl), 
+        SCMP_SYS(fstatfs), SCMP_SYS(futex), SCMP_SYS(getcwd), SCMP_SYS(getegid), 
+        SCMP_SYS(geteuid), SCMP_SYS(getgid), SCMP_SYS(getpid), SCMP_SYS(getrandom), 
+        SCMP_SYS(gettid), SCMP_SYS(getuid), SCMP_SYS(ioctl), SCMP_SYS(lseek), 
+        SCMP_SYS(madvise), SCMP_SYS(mmap), SCMP_SYS(mprotect), SCMP_SYS(mremap), 
+        SCMP_SYS(munmap), SCMP_SYS(newfstatat), SCMP_SYS(openat), SCMP_SYS(prctl), 
         SCMP_SYS(pread64), SCMP_SYS(preadv), SCMP_SYS(prlimit64), SCMP_SYS(pwrite64), 
-        SCMP_SYS(pwritev), SCMP_SYS(read), SCMP_SYS(readv), SCMP_SYS(rseq), 
-        SCMP_SYS(rt_sigaction), SCMP_SYS(rt_sigprocmask), SCMP_SYS(rt_sigreturn), 
-        SCMP_SYS(set_robust_list), SCMP_SYS(set_tid_address), SCMP_SYS(statfs), 
-        SCMP_SYS(statx), SCMP_SYS(write), SCMP_SYS(writev)
+        SCMP_SYS(pwritev), SCMP_SYS(read), SCMP_SYS(readv), SCMP_SYS(readlinkat), 
+        SCMP_SYS(rseq), SCMP_SYS(rt_sigaction), SCMP_SYS(rt_sigprocmask), 
+        SCMP_SYS(rt_sigreturn), SCMP_SYS(set_robust_list), SCMP_SYS(set_tid_address), 
+        SCMP_SYS(sigaltstack), SCMP_SYS(statfs), SCMP_SYS(statx), SCMP_SYS(sysinfo), 
+        SCMP_SYS(uname), SCMP_SYS(write), SCMP_SYS(writev)
     }},
     {language_enum::CPP, {
-        SCMP_SYS(brk), SCMP_SYS(close), SCMP_SYS(exit), SCMP_SYS(exit_group),
-        SCMP_SYS(faccessat), SCMP_SYS(fcntl), SCMP_SYS(fstatfs), SCMP_SYS(futex), 
-        SCMP_SYS(getpid), SCMP_SYS(getrandom), SCMP_SYS(gettid), SCMP_SYS(ioctl), 
-        SCMP_SYS(lseek), SCMP_SYS(madvise), SCMP_SYS(mmap), SCMP_SYS(mprotect), 
-        SCMP_SYS(mremap), SCMP_SYS(munmap), SCMP_SYS(newfstatat), SCMP_SYS(openat), 
+        SCMP_SYS(brk), SCMP_SYS(clock_gettime), SCMP_SYS(close), SCMP_SYS(exit), 
+        SCMP_SYS(exit_group), SCMP_SYS(faccessat), SCMP_SYS(faccessat2), SCMP_SYS(fcntl), 
+        SCMP_SYS(fstatfs), SCMP_SYS(futex), SCMP_SYS(getcwd), SCMP_SYS(getegid), 
+        SCMP_SYS(geteuid), SCMP_SYS(getgid), SCMP_SYS(getpid), SCMP_SYS(getrandom), 
+        SCMP_SYS(gettid), SCMP_SYS(getuid), SCMP_SYS(ioctl), SCMP_SYS(lseek), 
+        SCMP_SYS(madvise), SCMP_SYS(mmap), SCMP_SYS(mprotect), SCMP_SYS(mremap), 
+        SCMP_SYS(munmap), SCMP_SYS(newfstatat), SCMP_SYS(openat), SCMP_SYS(prctl), 
         SCMP_SYS(pread64), SCMP_SYS(preadv), SCMP_SYS(prlimit64), SCMP_SYS(pwrite64), 
-        SCMP_SYS(pwritev), SCMP_SYS(read), SCMP_SYS(readv), SCMP_SYS(rseq), 
-        SCMP_SYS(rt_sigaction), SCMP_SYS(rt_sigprocmask), SCMP_SYS(rt_sigreturn), 
-        SCMP_SYS(set_robust_list), SCMP_SYS(set_tid_address), SCMP_SYS(statfs), 
-        SCMP_SYS(statx), SCMP_SYS(write), SCMP_SYS(writev)
+        SCMP_SYS(pwritev), SCMP_SYS(read), SCMP_SYS(readv), SCMP_SYS(readlinkat), 
+        SCMP_SYS(rseq), SCMP_SYS(rt_sigaction), SCMP_SYS(rt_sigprocmask), 
+        SCMP_SYS(rt_sigreturn), SCMP_SYS(set_robust_list), SCMP_SYS(set_tid_address), 
+        SCMP_SYS(sigaltstack), SCMP_SYS(statfs), SCMP_SYS(statx), SCMP_SYS(sysinfo), 
+        SCMP_SYS(uname), SCMP_SYS(write), SCMP_SYS(writev)
     }},
     {language_enum::RUST, {
         SCMP_SYS(brk), SCMP_SYS(clock_gettime), SCMP_SYS(clock_nanosleep), 
