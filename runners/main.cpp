d68e0a12fb1d722172580f1207a8df76959eee90
s0chu <didididi328@gmail.com>
2026-06-02 12:23:23 +0300

    done

--- runners/main.cpp
+++ runners/main.cpp
@@ -170,6 +170,12 @@ void receive_request(int client_fd)
     if(request_state_table.count(client_fd) == 0)
         return;
 
+    if(*request_state_table[client_fd] == nullptr)
+    {
+        rem_fd(client_fd);
+        request_state_table.erase(client_fd);
+    }  
+    
     (*request_state_table[client_fd]) -> add();
 
     if(*request_state_table[client_fd] == nullptr)
